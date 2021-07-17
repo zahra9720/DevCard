ba74f836256706b42e2f2bb0e7362d897476966b
zahra jebeli <zahra.jebeli79@gmail.com>
2021-07-18 00:53:25 +0430

    projects section from page moved to partial view Projects

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Tiltle { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+}
