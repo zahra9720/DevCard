94dc84734a0fab954aac1c2b44e788d930cb7c02
zahra jebeli <zahra.jebeli79@gmail.com>
2021-07-19 00:46:47 +0430

    Projects & Latest Articles Moved to View Component

--- DevCard_MVC/Models/Article.cs
+++ DevCard_MVC/Models/Article.cs
@@ -2,8 +2,16 @@
 {
     public class Article
     {
+        public Article(long id , string tiltle, string description, string image)
+        {
+            Id = id;
+            Tiltle = tiltle;
+            Description = description;
+            Image = image;
+        }
         public long Id { get; set; }
         public string Tiltle { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
     }
 }
